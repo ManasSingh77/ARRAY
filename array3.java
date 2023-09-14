90b361526a296070cfedea3e47846df8d4e53d1b
Manas Singh <122530839+ManasSingh77@users.noreply.github.com>
2023-09-14 15:36:15 +0530

    Add files via upload

---
+++ array3.java
@@ -0,0 +1,17 @@
+package ARRAY;
+
+public class array3 {
+
+	public static void main(String[] args) {
+		int[] arr= {2,3,4,5,7};
+		System.out.println(arr[0]+" "+arr[1]);
+		Swap(arr,0,1);
+		System.out.println(arr[0]+" "+arr[1]);
+	}
+	public static void Swap(int[] arr, int i,int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+
+}
